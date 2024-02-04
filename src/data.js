0574f00cd811ce97221ecf1124b8c2868c96a383
NuwanHathurusinghe <chamodnuwan23@gmail.com>
2024-02-04 07:10:54 -0800

    Added data

---
+++ src/data.js
@@ -0,0 +1,21 @@
+import img1 from './statics/1.png'
+import img2 from './statics/2.png'
+import img5 from './statics/3.png'
+import img6 from './statics/6.png'
+import img3 from './statics/4.png'
+import img4 from './statics/5.png'
+
+export const data = [
+    {id:"0",
+imglink:img1,},
+{id:"1",
+imglink:img2,},
+{id:"2",
+imglink:img3,},
+{id:"4",
+imglink:img4,},
+{id:"5",
+imglink:img5,},
+{id:"6",
+imglink:img6,}
+]
